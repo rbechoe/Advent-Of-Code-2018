cdc39ef43ddd248b20d6a8505ff7457c661bcce1
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2021-12-11 15:02:51 +0100

    crazy performance gain

--- Assets/Scripts/ten.cs
+++ Assets/Scripts/ten.cs
@@ -69,23 +69,26 @@ public class ten : MonoBehaviour
             velocities.Add(velo);
         }
 
-        st.Stop();
+        // I timed the stopwatch first at this exact line, before the animations hence the short time
         // first run A https://gyazo.com/a5aafd3ae4ee83e997611038ab9a7397
         // 10086 updates for https://gyazo.com/161c112e61ef9ed353760a1cba6ac966 OH SHIT THIS WAS PART TWO!!
-        UnityEngine.Debug.Log(string.Format("took {0} ms to complete", st.ElapsedMilliseconds));
-        yield return new WaitForEndOfFrame();
 
         // visualize data x amount of times with waiting times for answer since this will use wait for seconds it is outside of stopwatch
-        for (int i = 0; i < 10086; i++)
+        for (int i = 0; i < objects.Count; i++)
         {
-            for (int j = 0; j < objects.Count; j++)
-            {
-                positions[j] += velocities[j];
-                objects[j].transform.position = positions[j];
-            }
-            yield return new WaitForEndOfFrame();
+            positions[i] += velocities[i] * 10086;
+            objects[i].transform.position = positions[i];
         }
+
         animating = false;
+
+        // Run A https://gyazo.com/4d915d65af149b42a5748a63b489f7d1
+        // Run B https://gyazo.com/9cf4517caafc4024bb455dad9016286f
+        // Run C https://gyazo.com/246d881d3db5c3eceae23db86b0edb31
+        // Run D https://gyazo.com/c7177346ad922cc7df88e541c5de86ad
+        st.Stop();
+        UnityEngine.Debug.Log(string.Format("took {0} ms to complete", st.ElapsedMilliseconds));
+        yield return new WaitForEndOfFrame();
     }
 
     IEnumerator DoingAnimation()
